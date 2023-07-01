4927c8ea6018b176b16044014a112c505179de63
daniel.manoel <daniellfmanoel@gmail.com>
2023-07-01 16:20:35 -0300

    sidebar completa

--- feed-ignite/src/App.js
+++ feed-ignite/src/App.js
@@ -1,15 +1,19 @@
 import './global.css';
 import styles from './App.module.css';
-import {Header} from './components/header/Header';
-import {Sidebar} from './components/sidebar/Sidebar';
+import { Header } from './components/header/Header';
+import { Sidebar } from './components/sidebar/Sidebar';
+import { Post } from './components/post/Post'
 
 function App() {
   return (
     <div>
       <Header />
-      <div className = {styles.wrapper}>
+      <div className={styles.wrapper}>
         <Sidebar />
-        <main><h1>Teste</h1></main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
